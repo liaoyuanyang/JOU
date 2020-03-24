6b97889e434effe13a0800485ccd1346df154c28
YuanyangLiao <1262607947@qq.com>
2020-03-25 00:59:14 +0800

    简单完成移动端静页面

--- server/index.js
+++ server/index.js
@@ -9,8 +9,12 @@ const app = express() // 创建一个网站服务器
 app.use(require('cors')()) // 允许跨域请求
 app.use(express.json()) // 允许处理post请求
 
+// 将 uploads 文件夹开放为静态资源
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
+
 require('./plugins/db')(app) // 引入连接数据库的方法
 require('./routes/admin')(app) // 引入admin的路由方法
+require('./routes/web')(app) // 引入admin的路由方法
 
 app.use('/public', express.static(__dirname + '/public')) // 开放public为静态资源
 
--- server/routes/admin/index.js
+++ server/routes/admin/index.js
@@ -1,39 +1,46 @@
 module.exports = app => {
     const express = require('express')
     const router = express.Router() // 创建express的子路由
-    const Category = require('../../models/Category')
 
-    // 添加分类
-    router.post('/categories', async(req, res) => {
-        const model = await Category.create(req.body)
+    // 添加资源
+    router.post('/', async(req, res) => {
+        const model = await req.model.create(req.body)
         res.send(model)
     })
 
-    // 修改分类
-    router.put('/categories/:id', async(req, res) => {
-        const model = await Category.findByIdAndUpdate(req.params.id, req.body)
+    // 修改资源
+    router.put('/:id', async(req, res) => {
+        const model = await req.model.findByIdAndUpdate(req.params.id, req.body)
         res.send(model)
     })
 
-    // 删除分类
-    router.delete('/categories/:id', async(req, res) => {
-        const model = await Category.findByIdAndDelete(req.params.id, req.body)
+    // 删除资源
+    router.delete('/:id', async(req, res) => {
+        const model = await req.model.findByIdAndDelete(req.params.id, req.body)
         res.send({
             success: true
         })
     })
 
-    // 查找所有分类
-    router.get('/categories', async(req, res) => {
-        const model = await Category.find().populate('parent').limit(50)
+    // 查找所有资源
+    router.get('/', async(req, res) => {
+        const queryOptions = {}
+        if (req.Model.modelName === 'Category') {
+            queryOptions.populate = 'parent'
+        }
+        const model = await req.Model.find().setOptions(queryOptions)
         res.send(model)
     })
 
-    // 根据id查找分类
-    router.get('/categories/:id', async(req, res) => {
-        const model = await Category.findById(req.params.id)
+    // 根据id查找资源
+    router.get('/:id', async(req, res) => {
+        const model = await req.model.findById(req.params.id)
         res.send(model)
     })
 
-    app.use('/admin/api', router) // 挂载子路由到qpp上
+    app.use('/admin/api/rest/:resource', async(req, res, next) => {
+        const modelName = require('inflection').classify(req.params.resource)
+        req.model = require(`../../models/${modelName}`)
+        next()
+    }, router)
 }
